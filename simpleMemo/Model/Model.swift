c871b44689a416e47e2871f78d62ed5564f4a36e
HHJ-S2 <berdle23@knou.ac.kr>
2022-03-29 23:32:59 +0900

    add: 테이블 뷰 레이아웃 구현

---
+++ simpleMemo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  simpleMemo
+//
+//  Created by hhj on 2022/03/29.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String){
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "첫번째 메모 입니다."),
+        Memo(content: "두번째 메모 입니다."),
+    ]
+}

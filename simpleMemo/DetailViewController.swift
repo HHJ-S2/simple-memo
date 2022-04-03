67ea76472106967a714afc1b94505ee8e711da71
HHJ-S2 <berdle23@knou.ac.kr>
2022-04-03 22:30:13 +0900

    add: 메모 보기 레이아웃 구현

--- simpleMemo/DetailViewController.swift
+++ simpleMemo/DetailViewController.swift
@@ -27,3 +27,31 @@ class DetailViewController: UIViewController {
     */
 
 }
+
+extension DetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    // 테이블 뷰가 어떤 셀을 표시할지 지정
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row{
+            
+        // 첫번째 셀에는 메모 텍스트를 표시
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+            return cell
+            
+        // 두번째 셀에는 날짜를 표시
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+            return cell
+            
+        // 표시할 셀을 지정하지 않았을땐 크래시
+        default:
+            fatalError()
+        }
+    }
+    
+    
+}
